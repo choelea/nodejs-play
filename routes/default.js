f3a73b7747530a7b615b87b25002e3255e04420b
Joe Li <joe.li@okchem.com>
2017-07-14 16:53:05 +0800

    Add login form page

--- routes/default.js
+++ routes/default.js
@@ -4,6 +4,8 @@ let router  = require('express').Router();
 router.get('/', (req, res) => {
     res.render('home');
 });
-
+router.get('/login',(req, res) => {
+    res.render('login');
+});
 
 module.exports = router;
