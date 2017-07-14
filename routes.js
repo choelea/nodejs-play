ddb3f096b8880ac508e4018edff827078e2ba088
Joe Li <joe.li@okchem.com>
2017-07-14 16:48:43 +0800

    Add session and session demo page

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@
 function create(app) {
     app.use('/',         require('./routes/default'));
     app.use('/images',    require('./routes/images'));
-    app.use('/about',    require('./routes/about'));
+    app.use('/session-demo',    require('./routes/session-demo'));
 
     app.use((req, res, next) => {
         res.status(404);
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 //to view es6 capabilities see http://node.green/
 //node v8-options es6 module syntax currently under development (2016/06/25)
-let path         = require('path');
-let express      = require('express');
+let path = require('path');
+let express = require('express');
 let cookieParser = require('cookie-parser');
-let bodyParser   = require('body-parser');
-let loki         = require('lokijs');
-let routes       = require('./routes');
-var hbs          = require('hbs');
+let bodyParser = require('body-parser');
+let loki = require('lokijs');
+let routes = require('./routes');
+var hbs = require('hbs');
+const session = require('express-session');
 
 //setup
 let database = new loki('database.loki', { autoload: true, autosave: true });
-let app      = express();
+let app = express();
 
 //settings
 app.set('port', process.env.PORT || 3000);
@@ -23,6 +24,7 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: 'xiaocheng', resave: false, saveUninitialized: false }));
 app.use('/public', express.static('public'));
 app.use('/uploads', express.static('uploads'));
 //loki db reference for the router
@@ -33,11 +35,11 @@ routes.create(app);
 
 // error handler
 app.use(function (err, req, res, next) {
-  if(err.code && err.code=='LIMIT_FILE_COUNT'){
-      console.log(JSON.stringify(err));
-      res.status(400).json(err);
-  }else{
-      console.log(err);
+  if (err.code && err.code == 'LIMIT_FILE_COUNT') {
+    console.log(JSON.stringify(err));
+    res.status(400).json(err);
+  } else {
+    console.log(err);
     res.status(500).send('Something broke!')
   }
 })
