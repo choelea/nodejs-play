2f06cb2bb58375da26f46d90d222ad2737aa88a3
Joe Li <joe.li@okchem.com>
2017-09-04 15:07:27 +0800

    Add test post

--- routes/default.js
+++ routes/default.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) => {
     res.render('home');
 });
 
+app.post('/demo/form', recaptcha.middleware.verify, function(req, res){
+    console.log(req.recaptcha);
+});
+
 module.exports = router;
